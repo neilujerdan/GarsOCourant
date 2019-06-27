adb521fdd250cb038503c57ffb1676b17d9fb535
neilujerdan <julien.letouzic1@gmail.com>
2019-06-27 15:48:39 +0200

    fix

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -3,7 +3,6 @@ include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe(
   access_token: ENV['ACCESS_TOKEN'],
-  subscribed_fields: %w[feed mention name]
 )
 
 Bot.on :message do |message|

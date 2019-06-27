449a9eda4a576908a7449c98bb6771a9e33e0d52
neilujerdan <julien.letouzic1@gmail.com>
2019-06-27 17:15:06 +0200

    fix

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -7,36 +7,5 @@ Facebook::Messenger::Subscriptions.subscribe(
 )
 
 Bot.on :message do |message|
-
-  #consoEnergie = 0   
   message.reply(text: 'Bienvenue sur SunCherBot, souhaitez-vous des conseils pour mieux consommer votre énergie solaire ?')
-  
-  /if message.text.include? "oui" ?: "Oui" ?: "OUI"
-      message.reply(text: "Possédez vous des panneaux solaire ?")
-  elsif message.text.include? "NON" ?: "Non" ?: "non"
-      message.reply(text: "D'accord n'hésitez a revenir me voir plus tard.")
-  else 
-      message.reply(text: "je n'ai pas compris ta réponse")
-  end
-
-  if message.text.include? "oui" ?: "Oui" ?: "OUI"
-      message.reply(text: "Possédez vous un ou des appareil suivant ? 
-      Un four éléctrique, une plaque éléctrique, une machine a lavez, un lave vaiselle, un sèche linge.")
-  elsif message.text.include? "NON" ?: "Non" ?: "non"
-      message.reply(text: "Vous avez la possibilité de produire et partagez votre électricité à travers le réseau public 
-      et faire une économie sur le cout de votre énérgie d'environ 30% grace au service SunShare. 
-      Vous pouvez consultez leur site web pour plus d'information -> https://www.sunshare.fr")
-  else 
-      message.reply(text: "je n'ai pas compris ta réponse")
-  end
-  
-  appareil = message.text
-
-  message.reply(text: "Quand utilisez vous vos appareil habituellement ? Le matin, le midi, le soir")
-
-  utilisation = message.text
-
- # if message.text.include? "four electrique" ?: "four éléctrique" ?: "Four éléctrique" ?: "Four electrique"
-      consoEnergie = consoEnergie + /
-
 end
